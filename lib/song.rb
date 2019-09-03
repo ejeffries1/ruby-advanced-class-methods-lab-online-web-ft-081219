d341a2d8eddd9742e3b7143d7922dd43702a86e4
Evan Jeffries <ejeffries14@gmail.com>
2019-09-03 09:24:36 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -35,7 +35,7 @@ class Song
   
   def self.new_from_filename(file)
     song = self.new
-    song.name = file.split(" ")
+    song.name = file.split(" ")[1]
     song.artist_name = file.split(" - ")[0]
     song
   end
