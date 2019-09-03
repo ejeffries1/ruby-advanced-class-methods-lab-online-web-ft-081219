0d2b0ea94ee73e3367d12c6c4acb1f95d6808a7a
Evan Jeffries <ejeffries14@gmail.com>
2019-09-03 09:17:48 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,42 @@
 class Song
   attr_accessor :name, :artist_name
   @@all = []
+  
+  def self.create
+   song = self.new
+   @@all << song
+    song
+  end
+  
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+  
+  def self.create_by_name(name)
+    song = self.new
+    song.name = name
+    @@all << song
+    song
+  end
+  
+  def self.find_by_name(name)
+     @@all.find{|songs|songs.name == name}
+  end
+  
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) || self.create_by_name(name)
+  end
+  
+  def self.alphabetical
+    @@all.sort_by{ |s| s.name}
+  end
 
   def self.all
     @@all
   end
-
+  
   def save
     self.class.all << self
   end
